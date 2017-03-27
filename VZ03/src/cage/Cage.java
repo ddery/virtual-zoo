db129d3556e086482996d44b8160dc0abc5b8526
faizghifari <fgh1997@live.com>
2017-03-27 17:58:01 +0700

    minor changes

---
+++ VZ03/src/cage/Cage.java
@@ -0,0 +1,24 @@
+package cage;
+
+import cell.Cell;
+import renderable.Renderable;
+
+/**
+ * @author      Faiz Haznitrama <13515010@std.stei.itb.ac.id>
+ * @version     1.0
+ * @since       1.0
+ */
+public class Cage implements Renderable {
+    /**
+     * cageNumber define the cage number
+     */
+    private int cageNumber;
+    /**
+     * size define the
+     */
+    private int size;
+    private int nAnimal;
+    private static int nbCage;
+    private Cell [] cellInside;
+    public void render(){}
+}

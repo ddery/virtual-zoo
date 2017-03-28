219cc3212e13dd88eb420c6becb035fdac84e223
Dery Rahman A <dery.ra@gmail.com>
2017-03-28 12:18:49 +0700

    Ubah format type cell

--- VZ03/src/cell/Cell.java
+++ VZ03/src/cell/Cell.java
@@ -1,15 +1,17 @@
 package cell;
 import animal.*;
+import util.Global;
+
 /**
  * @author      Faiz Haznitrama <13515010@std.stei.itb.ac.id>
- * @version     1.0
+ * @version     1.1
  * @since       1.0
  */
 public abstract class Cell {
     /**
      * type define what kind of cell it is
      */
-    private char type;
+    private short type;
     /**
      * hewan is a pointer to an Animal object that
      * represent an animal that exist in a cell
@@ -19,7 +21,7 @@ public abstract class Cell {
      * Get type of cell
      * @return return the type of cell
      */
-    public char getType() {
+    public short getType() {
         return type;
     }
     /**
@@ -31,10 +33,10 @@ public abstract class Cell {
     }
     /**
      * Set a new type for the cell
-     * @param new_type : as new type that is going to be assigned
+     * @param newType : as new type that is going to be assigned
      */
-    protected void setType(char new_type) {
-        type = new_type;
+    protected void setType(short newType) {
+        type = newType;
     }
     /**
      * Set a new pointer to Animal for the cell
@@ -44,60 +46,12 @@ public abstract class Cell {
         hewan = A;
     }
     /**
-     * Validate a Land type cell
-     * @return True if cell type is Land, false if not
+     * Validate a current cell
+     * @param type : type cell to compare with current cell
+     * @return True if current cell type is type, false if not
      */
-    public boolean isLH() {
-        return (type == 'L');
-    }
-    /**
-     * Validate a Water type cell
-     * @return True if cell type is Water, false if not
-     */
-    public boolean isWH() {
-        return (type == 'W');
-    }
-    /**
-     * Validate an Air type cell
-     * @return True if cell type is Air, false if not
-     */
-    public boolean isAH() {
-        return (type == 'A');
-    }
-    /**
-     * Validate a Restaurant type cell
-     * @return True if cell type is Restaurant, false if not
-     */
-    public boolean isRestaurant() {
-        return (type == 'R');
-    }
-    /**
-     * Validate a Park type cell
-     * @return True if cell type is Park, false if not
-     */
-    public boolean isPark() {
-        return (type == 'P');
-    }
-    /**
-     * Validate a Road type cell
-     * @return True if cell type is Road, false if not
-     */
-    public boolean isRoad() {
-        return (type == 'r');
-    }
-    /**
-     * Validate an Entrance type cell
-     * @return True if cell type is Entrance, false if not
-     */
-    public boolean isEntrance() {
-        return (type == 'E');
-    }
-    /**
-     * Validate an Exit type cell
-     * @return True if cell type is Exit, false if not
-     */
-    public boolean isExit() {
-        return (type == 'e');
+    public boolean cekTypeCell(short type) {
+        return (type & this.type) > 0;
     }
     /**
      * Validate whether there is an Animal in the cell
